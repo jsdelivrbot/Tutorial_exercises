b12c04ce42cc0131a2ebabb1027ec29d3df4b21f
charlie <charliestorey103@gmail.com>
2016-11-19 23:42:04 -0500

    ADD: Book model functions added

--- JS/Vanilla/trainingManager/src/model/Book.js
+++ JS/Vanilla/trainingManager/src/model/Book.js
@@ -23,12 +23,19 @@ function Book ( details ) {
 
 }
 
+//Regular expression object holder
 RegEx = {
   ISBN: /\b\d{9}(\d|X)/
 };
 
+// Temproary Book object holder
 Book.instances = {};
 
+/**
+* Check constraints of ISBN
+* @param ISBN number
+* @return false | constraint violation
+*/
 Book.checkIsbn = function(id){
   if(!id){
     return new NoConstraintViolation();
@@ -38,6 +45,52 @@ Book.checkIsbn = function(id){
       "ISBN cannot be an empty string!");
   }
   else if( !RegEx.ISBN.test(id) ){
-    return new 
+    return new PatternConstraintValidation(
+      " ISBN format incorrect");
   }
+  else{
+    return new NoConstraintViolation();
+  }
+};
+
+/**
+* Return constraint violation based on property or unique ISBN failure
+* @param false | constraint violation object
+* @return constraint violation
+*/
+Book.checkIsbnAsId = function (id) {
+  var constraintViolation = Book.checkIsbn( id);
+  if ((constraintViolation instanceof NoConstraintViolation))
+  {
+    if (!id) {
+      constraintViolation = new MandatoryValueConstraintViolation(
+          "A value for the ISBN must be provided!");
+    } else if (Book.instances[id]) {
+      constraintViolation = new UniquenessConstraintViolation(
+          "There is already a book record with this ISBN!");
+    } else {
+      constraintViolation = new NoConstraintViolation();
+    }
+  }
+  return constraintViolation;
+};
+
+/*
+*
+*/
+Book.prototype.setIsbn = function(id){
+  var validationResult = Book.checkIsbnAsId(id);
+  if (validationResult instanceof NoConstraintViolation) {
+  this.isbn = id;
+} else {
+  throw validationResult;
+}
+};
+
+/*
+* Overide tostring function
+*/
+Book.prototype.toString = function () {
+  return "Book{ ISBN:" + this.isbn + ", title:" +
+      this.title + ", year:" + this.year +"}";
 };
