7e8c1b24de6f4e46d7f2aeeea8c3ee5c0cb4ee25
charliestorey103@gmail.com <charliestorey103@gmail.com>
2017-02-06 20:04:45 -0500

    ADD: Electron scotch player base and component files added

--- JS/Electron/BasicElectronApp/process/js/render.js
+++ JS/Electron/BasicElectronApp/process/js/render.js
@@ -1,18 +1,27 @@
+/******************************************************************************
+* MAIN REACT INJECTION
+* --------------------
+*
+*
+*******************************************************************************/
+
+//Import app3 modules
 var React = require('react');
 var ReactDOM = require('react-dom');
 var $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
 
+//Import app data
+var fs = eRequire('fs');
+var loadApts = JSON.parse(fs.readFileSync(dataLocation));
+
+//Import app modules
+var Main = require('./Main');
+
+
 $(function(){
   $('#petAppointments').append('<h3 class="text-success">Pet app loaded</h3>');
 });
 
-const MainInterface = React.createClass({
-  render: function(){
-    return(
-      <h1> React Component : Pet Wisdom </h1>
-    );
-  }
-})
-
-ReactDOM.render(<MainInterface/>, document.getElementById('petAppointments'));
+//Render to the DOM
+ReactDOM.render(<Main/>,document.getElementById('petAppointments'));
